96cf289636e021d4682533bb507ef0a29fe94903
orangeberry <orangeberry@kakao.com>
2021-02-22 02:46:25 +0900

    finish

--- main.py
+++ main.py
@@ -6,30 +6,45 @@ from selenium.webdriver.support.ui import WebDriverWait
 from webdriver_manager.chrome import ChromeDriverManager
 
 
-KEYWORD = "buy domain"
+class GoogleKeywordScreenshooter:
+    def __init__(self, keyword, screenshots_dir):
+        self.browser = webdriver.Chrome(ChromeDriverManager().install())
+        self.keyword = keyword
+        self.screenshots_dir = screenshots_dir
 
-browser = webdriver.Chrome(ChromeDriverManager().install())
+    def start(self):
+        self.browser.get("https://google.com")
+        search_bar = self.browser.find_element_by_class_name("gLFyf")
+        search_bar.send_keys(self.keyword)
+        search_bar.send_keys(Keys.ENTER)
+        try:
+            shitty_element = WebDriverWait(self.browser, 6).until(
+                EC.presence_of_element_located((By.CLASS_NAME, "g-blk"))
+            )
+            self.browser.execute_script(
+                """
+            const shitty = arguments[0,1];
+            shitty.parentElement.removeChild(shitty)
+            """,
+                shitty_element,
+            )
 
-browser.get("https://google.com")
+        except Exception:
+            pass
+        search_results = self.browser.find_elements_by_class_name("g")
 
-search_bar = browser.find_element_by_class_name("gLFyf")
+        for index, search_result in enumerate(search_results):
+            search_result.screenshot(
+                f"{self.screenshots_dir}/{self.keyword}x{index}.png"
+            )
 
-search_bar.send_keys(KEYWORD)
-search_bar.send_keys(Keys.ENTER)
-
-shitty_element = WebDriverWait(browser, 8).until(
-    EC.presence_of_element_located((By.CLASS_NAME, "g-blk"))
-)
-
-shitty_element.screenshot("shitty.png")
-""" search_results = browser.find_elements_by_class_name("g")
+    def finish(self):
+        self.browser.quit()
 
 
-    for index, search_result in enumerate(search_results):
-    class_name = search_result.get_attribute("class")
-    print(class_name)
-    if "kno-kp mnr-c g-blk" not in class_name:
-        search_result.screenshot(f"screenshots/{KEYWORD}x{index}.png")
-"""
-
-# browser.quit()
+domain_competitors = GoogleKeywordScreenshooter("tesla", "screenshots")
+domain_competitors.start()
+domain_competitors.finish()
+python_competitors = GoogleKeywordScreenshooter("apple", "screenshots")
+python_competitors.start()
+python_competitors.finish()
